f85601135980b8f5ce26467e0bf6d320bea84244
Shin Uozumi <sinozu@users.noreply.github.com>
2022-08-23 11:51:26 +0900

    add terraform

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
+.PHONY : asdf
 asdf:
 	/bin/bash asdf/install.sh
 
+.PHONY : brew
 brew:
 	/bin/bash brew/install.sh
 
+.PHONY : homeshick
 homeshick:
 	/bin/bash homeshick/install.sh
 
+.PHONY : osx
 osx:
 	/bin/bash osx/setting.sh
--- asdf/install.sh
+++ asdf/install.sh
@@ -2,6 +2,8 @@
 
 cd asdf
 
+cp .tool-versions ~/
+
 if ! (asdf which go > /dev/null 2>&1); then
 	asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 fi
@@ -13,6 +15,10 @@ fi
 
 if ! (asdf which java > /dev/null 2>&1); then
 	asdf plugin-add java
+fi
+
+if ! (asdf which terraform > /dev/null 2>&1); then
+	asdf plugin-add terraform
 fi
 
 asdf install
